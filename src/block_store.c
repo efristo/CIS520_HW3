3fac1de1fd1bdf4c939c207d5c41aac2087e8827
Evan Fristo <evfristo@eos.beocat.ksu.edu>
2023-04-02 12:58:12 -0500

    changes

--- src/block_store.c
+++ src/block_store.c
@@ -7,82 +7,40 @@
 // You might find this handy.  I put it around unused parameters, but you should
 // remove it before you submit. Just allows things to compile initially.
 #define UNUSED(x) (void)(x)
-#define BITMAP_START_BLOCK 127
 
-// struct defining what a block actually is
-// we went with uchar since each is 1 byte
-typedef struct block {
-    unsigned char block[BLOCK_SIZE_BYTES];
-} block_t; 
 
-// struct defining the block store
 typedef struct block_store 
 {
-    bitmap_t *fbm;
-    int *blocks[BLOCK_STORE_NUM_BLOCKS];
-    uint32_t bitmap_blocks;
+    void *blocks[BLOCK_STORE_NUM_BLOCKS]; // array of blocks
+    size_t size; // size of bs in bytes
 } block_store_t;
 
-// creates new block store device 
+
 block_store_t *block_store_create()
 {
-    block_store_t *bs = (block_store_t*)malloc(sizeof(block_store_t));
-    if (!bs)
-    {
-        return NULL; 
-    }
-
-    bs->fbm = bitmap_create(256);
-
-    uint32_t blocks_required = (BLOCK_STORE_NUM_BLOCKS / 8) / 32;
-    uint32_t i; 
-
-    for (i = BITMAP_START_BLOCK; i < BITMAP_START_BLOCK + blocks_required; i++) {
-        if (!block_store_request(bs, i)) break; 
-    }
-
-    bs -> bitmap_blocks = blocks_required;
-
-    if (bs -> fbm == NULL) return NULL;
+    // create the block storage
+    block_store_t *bs = malloc(sizeof(block_store_t));
+    if (!bs) return NULL;
     
+    // total # of bytes in block storage
+    bs->size = BLOCK_STORE_NUM_BYTES;
+
     return bs;
-
 }
-
 
 void block_store_destroy(block_store_t *const bs)
 {
-    
-    // checks that block store actually exists 
-    if (bs != NULL) {
-        bitmap_destroy(bs -> fbm);
-        // free block store memory 
+    if (bs) 
+    {
         free(bs);
     }
-
 }
 
 // Searches for free block, defines as in use, and returns the block number 
 size_t block_store_allocate(block_store_t *const bs)
 {
-    // checking parameters 
-    if (bs == NULL || bs -> fbm == NULL) {
-        return SIZE_MAX;
-    }
-
-    // looks for first zero bit address in the bitmap 
-    size_t block_number = bitmap_ffz(bs -> fbm);
-
-    // checking boundaries to make sure it is not out of bounds 
-    if (block_number >= (BLOCK_STORE_NUM_BLOCKS - bs -> bitmap_blocks) || block_number == SIZE_MAX) {
-        return SIZE_MAX;
-    }
-
-    // sets the requested bit 
-    bitmap_set (bs -> fbm, block_number);
-    
-    // returns the allocated block #
-    return block_number;
+    UNUSED(bs);
+    return 0;
 }
 
 bool block_store_request(block_store_t *const bs, const size_t block_id)
@@ -100,24 +58,14 @@ void block_store_release(block_store_t *const bs, const size_t block_id)
 
 size_t block_store_get_used_blocks(const block_store_t *const bs)
 {
-    // checking parameters 
-    if (bs == NULL || bs -> fbm == NULL) {
-        return SIZE_MAX;
-    }
-	
-	// return count of set bits in fbm
-    return bitmap_total_set(bs -> fbm);
+    UNUSED(bs);
+    return 0;
 }
 
 size_t block_store_get_free_blocks(const block_store_t *const bs)
 {
-    // checking parameters 
-    if (bs == NULL || bs -> fbm == NULL) {
-        return SIZE_MAX;
-    }
-	
-	// return count of non-set bits in fbm
-    return 256 - bitmap_total_set(bs -> fbm);
+    UNUSED(bs);
+    return 0;
 }
 
 size_t block_store_get_total_blocks()
