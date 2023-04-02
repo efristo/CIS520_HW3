a2b977e0edf1845a2b68752a24725486d77e71de
Evan Fristo <evfristo@eos.beocat.ksu.edu>
2023-04-01 21:23:48 -0500

    edited cmake file, should be able to test

---
+++ build/CMakeFiles/block_store.so.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/block_store.so.dir/src/block_store.c.o"
+  "libblock_store.so.a"
+  "libblock_store.so.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/block_store.so.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
